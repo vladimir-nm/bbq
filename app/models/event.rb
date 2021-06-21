93b087cfaabc5106cfed3bba91092828e14ba438
Vladimir Mordakin <mordakin.vn@yandex.ru>
2021-06-22 00:38:25 +0300

    fix delete event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
   has_many :subscribers, through: :subscriptions, source: :user
 
